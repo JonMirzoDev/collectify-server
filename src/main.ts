be6c4e9cb61d581d332cbc38d93788f7411d5ad3
JonMirzoDev <islomjon.swe@gmail.com>
2024-01-06 12:09:07 +0500

    cors issue solved

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Frontend server address
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
