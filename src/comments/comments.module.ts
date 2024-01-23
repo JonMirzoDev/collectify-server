2a63c1d129edcf38e5bbcfd401e9746d729af9a5
JonMirzoDev <islomjon.swe@gmail.com>
2024-01-23 16:46:40 +0500

    comments  functions created

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,11 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
+import { Item } from 'src/items/entities/item.entity';
+import { ItemsModule } from 'src/items/items.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment, Item]), ItemsModule],
   providers: [CommentsService],
   controllers: [CommentsController],
   exports: [TypeOrmModule],
