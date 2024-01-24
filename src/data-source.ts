6cc505e5d053bae58a74d37462c25c6757fc8133
JonMirzoDev <islomjon.swe@gmail.com>
2024-01-24 12:02:46 +0500

    datasource updated

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,10 +2,13 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { User } from './users/entities/user.entity';
 import { Item } from './items/entities/item.entity';
 import { Collection } from './collections/entities/collection.entity';
-import * as dotenv from 'dotenv';
 import { Like } from './likes/entities/like.entity';
 import { Comment } from './comments/entities/comment.entity';
+import * as dotenv from 'dotenv';
+
 dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -15,14 +18,10 @@ export const dataSourceOptions: DataSourceOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [User, Item, Collection, Like, Comment],
-  synchronize: true,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? {
-          rejectUnauthorized: false,
-        }
-      : false,
+  synchronize: !isProduction,
+  ssl: isProduction ? { rejectUnauthorized: false } : null,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
+
 export default dataSource;
